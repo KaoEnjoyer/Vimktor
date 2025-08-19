f34bc99639518148df1f8f280b09cb135442d7a0
Wiktor Ludwichowski <wiktorludwichowski@Wiktors-MacBook-Air.local>
2025-08-19 21:19:24 +0200

    new version of Vimktor

--- Makefile
+++
@@ -1,5 +0,0 @@
-main: 
-	g++ main.cpp filemanager.cpp renderer.cpp window.cpp -o vimktor -lncurses -std=c++17
-debug:
-	g++ main.cpp renderer.cpp window.cpp -o vimktor -lncurses -Wall -std=c++17
-
---
+++ vimktor.cpp
@@ -0,0 +1,91 @@
+#include "vimktor.h"
+#include <cstdint>
+#include <cstdio>
+#include <fstream>
+Vimktor::Vimktor() {}
+
+void Vimktor::Init() {
+  InitCurses();
+
+#ifdef DEBUG_MODE
+  LoadFile("test.txt");
+  logFile.open("log_file.txt", std::ios::out);
+#endif
+}
+
+void Vimktor::End() {
+#ifdef DEBUG_MODE
+  logFile.close();
+#endif
+  endwin();
+}
+
+VimktorErr_t Vimktor::InitCurses() {
+  initscr();
+  keypad(stdscr, true);
+  raw();
+  nodelay(stdscr, true);
+  noecho();
+  init_color(COLOR, 0, 0, 0);
+  return VIMKTOR_OK;
+}
+
+VimktorErr_t Vimktor::RenderWindow(WINDOW *window, uint16_t x, uint16_t y,
+                                   textBuffer &tb) {
+  uint16_t maxX, maxY;
+  getmaxyx(window, maxY, maxX);
+
+  if (maxY < y || maxX < x)
+    return INVALID_ARRGUMENT;
+
+  uint16_t height = maxY - y;
+  uint16_t width = maxX - x;
+
+  if (txt_offset_y > tb.size())
+    return MEMORY_ERROR;
+
+  for (int i = 0; i < height; i++) {
+
+    RenderLine(window, x, y + i, tb[txt_offset_y + i]);
+  }
+  return VIMKTOR_OK;
+}
+
+VimktorErr_t Vimktor::RenderLine(WINDOW *window, uint16_t x, uint16_t y,
+                                 const std::vector<glyph_t> &buff) {
+  int col = buff.size() - txt_offset_x;
+  for (int i = 0; i < col; i++) {
+    move(y, col + i);
+    waddch(window, buff[col + i].ch);
+    // TODO: add colors
+  }
+
+  return VIMKTOR_OK;
+}
+
+VimktorErr_t Vimktor::LoadFile(const std::string &fileName) {
+  std::fstream file;
+  file.open(fileName, std::ios::in);
+  if (!file.good()) {
+    return FILE_ERROR;
+  }
+
+  tb.clear();
+  tb.reserve(1000);
+  size_t line_num = 0;
+  std::string line;
+  while (file >> line) {
+    tb.push_back(std::vector<glyph_t>());
+
+    for (int i = 0; i < line.size(); i++) {
+
+      glyph_t g;
+      g.ch = line[i];
+      tb[line_num].push_back(g);
+    }
+
+    line_num++;
+  }
+
+  return VIMKTOR_OK;
+};
